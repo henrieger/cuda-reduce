77fda5b8d0b43896e1905996a02998cced02cd8b
Henrique Rieger <henriquerieger2001@gmail.com>
2024-07-06 13:50:15 -0300

    [add] kernel reduceMax_persist com 2 primeiras fases

--- reduce.cu
+++ reduce.cu
@@ -1,6 +1,6 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 #include <cuda_runtime.h>
 #include <helper_cuda.h>
@@ -12,26 +12,49 @@
 #define GPU GTX1080Ti
 
 #if GPU == GTX1080Ti
-    #define MP 28
-    #define THREADS_PER_BLOCK 1024
-    #define RESIDENT_BLOCKS_PER_MP 2
-    #define NTA (MP*RESIDENT_BLOCKS_PER_MP*THREADS_PER_BLOCK)
+#define MP 28
+#define THREADS_PER_BLOCK 1024
+#define RESIDENT_BLOCKS_PER_MP 2
+#define NTA (MP * RESIDENT_BLOCKS_PER_MP * THREADS_PER_BLOCK)
 #endif
 
 #if GPU == GTX1070
-    #define MP 28
-    #define THREADS_PER_BLOCK 1024
-    #define RESIDENT_BLOCKS_PER_MP 2
-    #define NTA (MP*RESIDENT_BLOCKS_PER_MP*THREADS_PER_BLOCK)
-#endif 
+#define MP 28
+#define THREADS_PER_BLOCK 1024
+#define RESIDENT_BLOCKS_PER_MP 2
+#define NTA (MP * RESIDENT_BLOCKS_PER_MP * THREADS_PER_BLOCK)
+#endif
 
-__global__ void reduce(const float *A, float *r) {
+__global__ void reduceMax_persist(float *max, float *Input,
+                                  unsigned int nElements) {
+  // Inicia vetor em shared memory com máximo de cada thread
+  __shared__ float threadsMax[THREADS_PER_BLOCK];
+  int t = threadIdx.x;
+  threadsMax[t] = 0;
+
+  // FASE 1 - Computa o máximo para cada thread
+  int initial = blockDim.x * blockIdx.x + t;
+  for (int i = initial; i < nElements; i += NTA)
+    threadsMax[t] = fmax(threadsMax[t], Input[i]);
+
+  // FASE 2 - Computa o máximo do bloco usando o algoritmo dos slides
+  for (int stride = blockDim.x; stride > 0; stride /= 2) {
+    __syncthreads();
+    if (t < stride)
+      threadsMax[t] = fmax(threadsMax[t], threadsMax[t + stride]);
+  }
+
+  // TODO: FASE 3 - Computa o máximo de todos os blocos usando atomicos
+}
+
+__global__ void reduceMax_atomic_persist(float *max, float *Input,
+                                         unsigned int nElements) {
   return;
 }
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    fprintf(stderr, "Uso: %s <TAM_VETOR>", argv[0]);
+    fprintf(stderr, "Uso: %s <TAM_VETOR>\n", argv[0]);
     exit(EXIT_FAILURE);
   }
 
@@ -51,7 +74,7 @@ int main(int argc, char **argv) {
 
   // Inicializa vetor A com valores aleatórios entre 0 e 1
   for (int i = 0; i < vectorSize; i++)
-    h_A[i] = (float)rand()/RAND_MAX;
+    h_A[i] = (float)rand() / RAND_MAX;
 
   // Copia vetor A para GPU
   cudaMemcpy(h_A, d_A, vectorSize * sizeof(float), cudaMemcpyHostToDevice);
@@ -62,7 +85,6 @@ int main(int argc, char **argv) {
   // Copia resultado para o host
   cudaMemcpy(&h_result, d_result, sizeof(float), cudaMemcpyDeviceToHost);
 
-
   // Calcula redução normal em CPU
   float correct = 0;
   for (int i = 0; i < vectorSize; i++)
@@ -70,7 +92,8 @@ int main(int argc, char **argv) {
 
   // Checa corretude do resultado
   if (h_result != correct) {
-    fprintf(stderr, "Resultado errado. Esperava %f e obteve %f", correct, h_result);
+    fprintf(stderr, "Resultado errado. Esperava %f e obteve %f\n", correct,
+            h_result);
     exit(EXIT_FAILURE);
   }
 
